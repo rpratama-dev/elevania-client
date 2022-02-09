82b9e7104bd66a0ffe3ee7abc8710cdb18ebaecc
Binasentra <rpratama.sky@gmail.com>
2022-02-09 19:35:39 +0700

    Change Router and Protect Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import './App.css';
-import { /* Routes, Route, */ useRoutes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import MainLayout from './layouts/main';
-// import AuthStore from './store/auth.store';
-// import { AdmDashboard, AdmManageProduct, AdmSyncProduct, Login, RequiredAuth } from './pages';
-// import { ProductDashboard } from './pages';
 import routes from './routes';
 import { observer } from 'mobx-react-lite';
 
-// const AuthRequired = (props) => {
-//   const { Component } = props;
-//   // * This component to protect routing
-//   return (
-//     <RequiredAuth {...props}>
-//       <Component {...props} />
-//     </RequiredAuth>
-//   );
-// };
-
 function App(props) {
-  // const authSstore = new AuthStore();
-
   const routing = useRoutes(routes(props.userStore.userLogin.isLogin, props));
   return (
     <>
